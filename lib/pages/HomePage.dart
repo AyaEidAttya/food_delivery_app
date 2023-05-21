2f485cdb1ca64d757e2b00da5b13d41ff27a5c27
aya <youremail@yourdomain.com>
2023-05-21 17:36:54 +0300

    creat appbar

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:delivery_app/widget/AppBar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+        children: [
+          AppBarWidget(),
+          
+        ],
+      ),
+    );
+  }
+}
