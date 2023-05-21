2f880181a5a23b9b730bd52f169bb8d0c9912a19
aya <youremail@yourdomain.com>
2023-05-21 18:14:16 +0300

    creat text field search to order

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:delivery_app/widget/AppBar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,10 +8,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
+      body: ListView(
         children: [
           AppBarWidget(),
-          
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(color: Colors.white,
+             borderRadius: BorderRadius.circular(20),
+             boxShadow:[ BoxShadow(blurRadius:10,
+             spreadRadius: 2,
+             color: Colors.grey.withOpacity(0.5),
+             offset: Offset(0, 3),
+              )]
+              
+              
+              ),
+              child: Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Icon(CupertinoIcons.search,color: Colors.red,),
+                 Container(
+                  height: 50,
+                  width: 300,
+                  child: Padding(padding: EdgeInsets.symmetric(horizontal: 15,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "what would you like to have ?",
+                       border: InputBorder.none,
+                    ),
+                  ),
+                  
+                  ),
+                 )
+                ],
+              ),
+              ),
+              
+            ),
+
+          ),
         ],
       ),
     );
